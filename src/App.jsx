ca1bdb0847d648216bc972434c72a2024ac209e2
taylorafox1 <taylorafox1@gmail.com>
2018-12-15 15:03:26 -0500

    added particles.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,43 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+      number: {
+        value: 150,
+        density: {
+          enable: true,
+          value_area: 800 
+        }
+      }
+    }
+  }
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+            params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
         {/* <FaceRecognition /> */}
       </div>
     );
