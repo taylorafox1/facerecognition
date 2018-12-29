f9bd573f7b41ef9602b6c0aae274a76cbd8aa43f
taylorafox1 <taylorafox1@gmail.com>
2018-12-29 17:45:55 -0500

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -87,7 +105,7 @@ class App extends Component {
         { route === 'home' ?
        <div>
        <Logo />
-       <Rank />
+       <Rank name={this.state.user.name} entries={this.state.user.entries}/>
        <ImageLinkForm 
          onInputChange={this.onInputChange} 
          onButtonSubmit={this.onButtonSubmit}
@@ -96,9 +114,9 @@ class App extends Component {
      </div> 
       : (
         route === 'signin' ?
-        <Signin onRouteChange={this.onRouteChange}/>
-        : 
-        <Register onRouteChange={this.onRouteChange}/>
+        <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+        :
+        <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
       )
      
         
