a93c1b643bb475ebb7949abe103bc31ef743c87d
taylorafox1 <taylorafox1@gmail.com>
2018-12-31 15:08:23 -0500

    connected to db

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -5,7 +5,7 @@ const Rank = ({name, entries}) => {
   return (
     <div >
       <div className='white f3'>
-        {'Andrei, your current rank is...'}
+        {`${name}, your current entry count is...`}
       </div>
       <div className='white f1'>
         {entries}
