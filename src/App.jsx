b2bbe662352cca75a1b82d137589428e5741e796
taylorafox1 <taylorafox1@gmail.com>
2019-01-01 13:39:49 -0500

    added register validation, removed APi key from header

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register'
 
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '14e4fcbf75ce4406a2604d567da50e68'
-})
 
 const particlesOptions = {
   particles: {
@@ -82,10 +77,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+      input: this.state.input
+        })
+      })
+      .then(response => response.json())
       .then(response => {
         if(response) {
           fetch('http://localhost:3000/image', {
