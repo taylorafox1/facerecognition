04408b01021a3fb9c0412987123d8f9d97be1e00
taylorafox1 <taylorafox1@gmail.com>
2019-01-01 11:56:35 -0500

    added catch to fetch

--- src/App.jsx
+++ src/App.jsx
@@ -99,6 +99,7 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
         })
+        .catch(console.log)
       }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
