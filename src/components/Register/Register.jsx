ab4b40e4085f3644be6bac63503fcce4533ca3e7
taylorafox1 <taylorafox1@gmail.com>
2019-01-02 11:55:58 -0500

    changed local host to heroku link

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -24,7 +24,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://sleepy-crag-34170.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
